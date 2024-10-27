41bbdb84908a576f3ab60ed99688a030d7a27c19
murali <muralinetha38@gmail.com>
2024-10-27 23:45:37 +0530

    html+css

---
+++ StudySync1.js
@@ -0,0 +1,14 @@
+alert("pop up");
+// prompt("what is your name");
+// var typeData=new Typed(".role",{
+//     Strings:[
+//         "Full Stack",
+//         "Web Dev",
+//         "Backend",
+//         "Coder",
+//     ],
+//     loop:true,
+//     typeSpeed:100,
+//     backSpeed:80,
+//     backDelay:1000,
+// });
